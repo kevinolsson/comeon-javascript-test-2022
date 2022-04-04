cb43e027340b7f29d59027c5cdbbd2cc84bfafc0
Kevin Olsson <kevinolsson305@gmail.com>
2022-04-04 10:14:55 +0200

    removed legal text

--- src/components/HomeSidebar/HomeSidebar.tsx
+++ src/components/HomeSidebar/HomeSidebar.tsx
@@ -6,16 +6,5 @@ export const HomeSidebar = (): JSX.Element => (
         <div className={classes.loginWrapper}>
             <Login />
         </div>
-        <div className={classes.legalText}>
-            <small>
-                Hos ComeOn! Sport &amp; Casino har du stora valmöjligheter. Vi
-                har en konstant växande lista över casino slots med favoriter
-                som exempelvis Starburst och Mega Fortune Dreams. Vi erbjuder
-                även uppskattade gamla kortspel med klassiker som Blackjack.
-                Dessutom hittar du alltid grymma bet &amp; odds på vår
-                Sportsida. Kika in våra <a href="#">casino guider</a> för mer
-                information.
-            </small>
-        </div>
     </div>
 );
