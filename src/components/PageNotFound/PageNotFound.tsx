2c5b75680edaaa3c02f9c7bb260600126db49e0e
Kevin <kevinolsson305@gmail.com>
2022-04-04 03:47:41 +0200

    not found

--- src/components/PageNotFound/PageNotFound.tsx
+++ src/components/PageNotFound/PageNotFound.tsx
@@ -1,1 +1,13 @@
-export const PageNotFound = () => <h1>Page not found</h1>;
+import classes from "./page-not-found.module.scss";
+import { Link } from "react-router-dom";
+import { paths } from "services/routes";
+
+export const PageNotFound = () => (
+    <div className={classes.root}>
+        <h1>Page not found</h1>
+        <p>
+            Head back <Link to={paths.index.path}>home</Link> and see if you
+            navigate from there!
+        </p>
+    </div>
+);
